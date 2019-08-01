c813c8717f0f7399c186e77c708d94ce3c2aa117
johsire <josiah5115@gmail.com>
2019-07-31 21:25:10 -0600

    nav element initial

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDom from "react-dom";
 
 // Create the app
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Fragment>
+      <nav className="navigation">
+        <a>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+            alt="image"
+          />
+        </a>
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+      <main>
+        <div className="leftContent">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+          <form>
+            <h2 className="formTitle">Add a bookmark</h2>
+            <div>
+              <label for="linkTitle" className="formLabel">
+                Enter a bookmark name
+              </label>
+              <input
+                type="text"
+                name="linkTitle"
+                minLength="1"
+                maxLength="25"
+                placeholder="25 characters max"
+              />
+            </div>
+            <div />
+            <button />
+          </form>
+        </div>
+        <div className="rightContent" />
+      </main>
+    </Fragment>
   );
 };
 
-ReactDom.render(
- <App />,
- document.getElementById("app")
-);
+ReactDom.render(<App />, document.getElementById("app"));
